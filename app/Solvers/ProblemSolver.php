bac11d554257b41e6d9daae43e16a4f730aab4a0
Linguisto Esteras <linguisto13@gmail.com>
2020-02-23 01:43:00 +0200

    thinking on the usefullness index

--- app/Solvers/ProblemSolver.php
+++ app/Solvers/ProblemSolver.php
@@ -38,30 +38,5 @@ abstract class ProblemSolver implements ProvidesSolution
     public function __construct(array $dataSet)
     {
         $this->dataSet = collect($dataSet);
-
-        $input = $this->dataSet->get(0);
-        $booksData = $this->dataSet->get(1);
-        $librariesData = $this->dataSet->slice(2)->chunk(2);
-
-        $books = collect();
-        foreach ($booksData as $id => $score) {
-            $books->put($id, new Book($id, $score));
-        }
-
-        $libraries = collect();
-
-        /**
-         * @var int $id
-         * @var Collection $libraryDatum
-         */
-        foreach ($librariesData as $id => $libraryDatum) {
-            $libraryBooks = $books->only($libraryDatum->last())->toArray();
-            $params = collect($libraryDatum->first())->prepend($id)->push($libraryBooks);
-            $libraries->push(new Library(...$params));
-        }
-
-        $this->libraries = $libraries->sortByDesc('signProcessDays');
-
-        $this->overAllDays = (int)Arr::last($input);
     }
 }
--- app/Solvers/WinnerSolver.php
+++ app/Solvers/WinnerSolver.php
@@ -1,11 +1,11 @@
 <?php
 
-
 namespace App\Solvers;
-
 
 use App\Objects\Book;
 use App\Objects\Library;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 
 /**
  * Class WinnerSolver
@@ -18,6 +18,53 @@ class WinnerSolver extends ProblemSolver
      * @var array
      */
     protected $result = [];
+
+    /**
+     * @var float
+     */
+    protected $scoreImportance;
+
+    /**
+     * @var float
+     */
+    protected $signProcessImportance;
+
+    /**
+     * WinnerSolver constructor.
+     *
+     * @param array $dataSet
+     */
+    public function __construct(array $dataSet)
+    {
+        parent::__construct($dataSet);
+
+        $input = $this->dataSet->get(0);
+        $booksData = $this->dataSet->get(1);
+        $librariesData = $this->dataSet->slice(2)->chunk(2);
+
+        $books = collect();
+        foreach ($booksData as $id => $score) {
+            $books->put($id, new Book($id, $score));
+        }
+
+        [$this->scoreImportance, $this->signProcessImportance] = $this->datasetRates($books);
+
+        $libraries = collect();
+
+        /**
+         * @var int $id
+         * @var Collection $libraryDatum
+         */
+        foreach ($librariesData as $id => $libraryDatum) {
+            $libraryBooks = $books->only($libraryDatum->last())->toArray();
+            $params = collect($libraryDatum->first())->prepend($id)->push($libraryBooks);
+            $libraries->push(new Library(...$params));
+        }
+
+        $this->libraries = $libraries->sortByDesc('signProcessDays');
+        $this->overAllDays = (int)Arr::last($input);
+    }
+
 
     /**
      * @inheritDoc
@@ -64,12 +111,46 @@ class WinnerSolver extends ProblemSolver
      */
     protected function usefulnessIndex(Library $library): float
     {
-        $effectiveBooksCount = $library->shipPerDay * ($this->overAllDays - $library->signProcessDays);
+        $effectiveDaysCount = $this->overAllDays - $library->signProcessDays;
+        $effectiveBooksCount = $library->shipPerDay * $effectiveDaysCount;
 
-        return $library
-                ->books
-                ->slice(0, $effectiveBooksCount)
-                ->sum('score') / $library->signProcessDays;
+        $libScore = $library->books->sum('score');
+        $effectiveLibScore = $library->books->slice(0, $effectiveBooksCount)->sum('score');
+
+        $libScoreSaldo = $libScore / $effectiveLibScore;
+        $libScore *= $libScoreSaldo;
+
+        $scoreRate = $libScore / $effectiveDaysCount;
+        $signProcessRate = $this->overAllDays / $library->signProcessDays;
+
+        $effectiveScoreRate = $effectiveBooksCount / ($scoreRate * $this->scoreImportance);
+        $effectiveSignProcessRate = $effectiveDaysCount / ($signProcessRate * $this->signProcessImportance);
+
+        return ($effectiveScoreRate + $effectiveSignProcessRate) / 2;
+    }
+
+    /**
+     * @param Collection $books
+     *
+     * @return array
+     */
+    protected function datasetRates(Collection $books): array
+    {
+        $maxBookScore = $books->max('score');
+        $averageScore = $books->avg('score');
+        $scoreImportanceCff = $maxBookScore - $averageScore;
+
+        while ($scoreImportanceCff > 1) {
+            $scoreImportanceCff /= 10;
+        }
+
+        $scoreImportance = 1 + $scoreImportanceCff;
+        $signProcessImportance = 1 - $scoreImportanceCff;
+
+        return [
+            $scoreImportance,
+            $signProcessImportance,
+        ];
     }
 
     /**
